5352aa8e521f1efc03b73c80d6c0f350bbfc69aa
Choihohee <lovelove4927@naver.com>
2024-07-10 13:28:15 +0900

    초기셋팅

---
+++ src/test/java/com/eatda/EatdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eatda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
