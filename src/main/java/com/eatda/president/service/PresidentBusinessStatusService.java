596ce35f4487d33f1022b25e372dfa5a1ff7054d
dongyeop00 <ehd_duq94@naver.com>
2024-08-01 11:59:36 +0900

    feat: 사업자번호 조회

---
+++ src/main/java/com/eatda/president/service/PresidentBusinessStatusService.java
@@ -0,0 +1,47 @@
+package com.eatda.president.service;
+
+import com.eatda.president.domain.PresidentBusinessRequest;
+import com.eatda.president.domain.PresidentBusinessResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+
+
+@Service
+@RequiredArgsConstructor
+public class PresidentBusinessStatusService {
+
+    @Value("${baseUrl}")
+    private String baseUrl;
+
+    @Value("${serviceKey}")
+    private String serviceKey;
+
+    public PresidentBusinessResponse getBusinessStatus(PresidentBusinessRequest request) {
+        String apiUrl = baseUrl + "serviceKey=" + serviceKey;
+
+        try {
+            URI uri = new URI(apiUrl);
+
+            RestTemplate restTemplate = new RestTemplate();
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+
+            HttpEntity<PresidentBusinessRequest> entity = new HttpEntity<>(request, headers);
+
+            ResponseEntity<PresidentBusinessResponse> response = restTemplate.exchange(uri, HttpMethod.POST, entity, PresidentBusinessResponse.class);
+
+            return response.getBody();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
