175d3776f9c52ed9eab4648cb4c0f895f7c72852
Choihohee <lovelove4927@naver.com>
2024-10-15 21:50:49 +0900

    refactor: 리뷰 등록, 조회 리팩토링 및 testcode 작성

--- src/main/java/com/eatda/review/repository/ReviewRepository.java
+++ src/main/java/com/eatda/review/repository/ReviewRepository.java
@@ -1,4 +1,11 @@
 package com.eatda.review.repository;
 
-public interface ReviewRepository {
+import com.eatda.review.domain.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    List<Review> findByRestaurantId(Long restaurantId);
 }
