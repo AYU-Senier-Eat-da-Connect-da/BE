c27b613c1b6e7bcf79b0011f8a49ba435d93e6e1
Choihohee <lovelove4927@naver.com>
2024-11-15 13:18:09 +0900

    최종

--- src/main/java/com/eatda/goodInfluenceStore/repository/GoodInfluenceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.eatda.goodInfluenceStore.repository;
-
-import com.eatda.goodInfluenceStore.domain.GoodInfluence;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GoodInfluenceRepository extends JpaRepository<GoodInfluence, Long> {
-}
